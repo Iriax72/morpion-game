d200e39a027170a7d66d6a6f4fbcbbac1275be36
flexult <touma.leandre.72@gmail.com>
2026-05-26 20:59:25 +0000

    commentaires

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
+<?php
 // index.php
 
 // PAGE ROUTEUR
 // pas de HTML
 // Redirection de l'user vers la bonne password_get_info
 
-header(Location: ./menu.php);
+header(Location: './menu.php');
 exit;
+?>
