b2f42419f08aee6bc5d8f8cdd22e04aed452f9c5
flexult <touma.leandre.72@gmail.com>
2026-05-26 20:48:28 +0000

    menu avec boutons

---
+++ index.php
@@ -0,0 +1,8 @@
+// index.php
+
+// PAGE ROUTEUR
+// pas de HTML
+// Redirection de l'user vers la bonne password_get_info
+
+header(Location: ./menu.php);
+exit;
---
+++ menu.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jouez au morpion !</title>
+    <link rel="stylesheet" href="menu.css">
+</head>
+<body>
+    <button id="create-game-btn" class="animated-btn"></button>
+    <button id="join-game-btn" class="animated-btn"></button>
+</body>
+</html>
