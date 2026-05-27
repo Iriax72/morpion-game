2bde3ad523e53fe76906b032bdb99c87b157f129
flexult <touma.leandre.72@gmail.com>
2026-05-27 06:48:51 +0000

    patch

--- menu.php
+++ menu.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jouez au morpion !</title>
-    <link rel="stylesheet" href="menu.css">
+    <link rel="stylesheet" href="./css/menu.css">
 </head>
 <body>
     <button id="create-game-btn" class="animated-btn">CRÉER UNE PARTIE</button>
