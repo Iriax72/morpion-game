ca7972725a165b753de28181aa722620f4628fb8
flexult <touma.leandre.72@gmail.com>
2026-05-28 13:38:00 +0000

    patch

--- index.php
+++ index.php
@@ -9,6 +9,11 @@ session_start();
 $_SESSION['user_id'] = bin2hex(random_bytes(16));
 
 require_once __DIR__ . '/config.php';
+try {
+    init_db();
+} catch (Throwable $e) {
+    die('<p style="color: red;">Erreur d\'initialisation de la base de données: ' . $e->getMessage() . '</p>');
+}
 
 header('Location: ./menu.php');
 exit;
